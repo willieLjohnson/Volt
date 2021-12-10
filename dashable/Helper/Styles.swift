52fbd4f80b090ba29f03b1674c86d40ff94b3ae5
Willie Johnson <liwa.johnson@gmail.com>
2021-12-10 17:36:37 -0500

    Update game default style

--- dashable/Helper/Styles.swift
+++ dashable/Helper/Styles.swift
@@ -9,12 +9,12 @@
 import SpriteKit
 
 struct Style {
-  static let BACKGROUND_COLOR: SKColor = #colorLiteral(red: 0.06274510175, green: 0, blue: 0.1921568662, alpha: 1)
-  static let GROUND_COLOR = #colorLiteral(red: 0.1764705926, green: 0.01176470611, blue: 0.5607843399, alpha: 1)
-  static let OBSTACLE_COLOR = #colorLiteral(red: 0.1764705926, green: 0.01176470611, blue: 0.5607843399, alpha: 1)
-  static let PLAYER_COLOR = #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)
-  static let PROJECTILE_COLOR = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)
+  static let BACKGROUND_COLOR: SKColor = #colorLiteral(red: 0.1921568662, green: 0.007843137719, blue: 0.09019608051, alpha: 1)
+  static let GROUND_COLOR = #colorLiteral(red: 0.3098039329, green: 0.01568627544, blue: 0.1294117719, alpha: 1)
+  static let OBSTACLE_COLOR = #colorLiteral(red: 0.4392156899, green: 0.01176470611, blue: 0.1921568662, alpha: 1)
+  static let PLAYER_COLOR = #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)
+  static let PROJECTILE_COLOR = #colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1)
   static let FLYER_COLOR = #colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1)
   static let CHASER_COLOR = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
-  static let BEE_COLOR =  #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)
+  static let BEE_COLOR =  #colorLiteral(red: 0.9568627477, green: 0.6588235497, blue: 0.5450980663, alpha: 1)
 }
