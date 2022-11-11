603cc72bdd8c4d4aeb89b6627c94003aa458fe40
Willie Johnson <liwa.johnson@gmail.com>
2022-11-11 07:47:05 -0500

    Fix audio

--- dashable/Game/GameViewController.swift
+++ dashable/Game/GameViewController.swift
@@ -18,8 +18,8 @@ class GameViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     if let view = self.view as! SKView? {
-      let game = GameWorld.startNewGame(size: view.bounds.size)
-      view.presentScene(game)
+      let gameWorld = GameWorld(game: GameScene(size: view.bounds.size))
+      view.presentScene(gameWorld.game)
       // Configure view
       view.ignoresSiblingOrder = true
       if debug {
--- dashable/Game/GameWorld.swift
+++ dashable/Game/GameWorld.swift
@@ -10,33 +10,16 @@ import Foundation
 import SpriteKit
 
 class GameWorld {
-  static var global = GameWorld(game: GameScene())
+  static var global: GameWorld!
+  var game: GameScene
   var canPlayAudio = true
-  
-  var game: GameScene?
   
   init(game: GameScene) {
     self.game = game
-  }
-  
-  static func startNewGame(size: CGSize) -> GameScene {
-    let game = GameWorld.newGame(size: size)
-    game.scaleMode = .aspectFill
-    GameWorld.global = GameWorld(game: game)
-    return game
-  }
-  
-  func restartGame() {
-    guard let game = game else { return }
-    self.game = GameWorld.newGame(size: game.size)
-  }
-  
-  static func newGame(size: CGSize) -> GameScene {
-    return GameScene(size: size)
+    GameWorld.global = self
   }
   
   func playAudio(_ fileName: String, duration: CGFloat, volume: Float, volumeChangeSpeed: CGFloat) {
-    guard let game = self.game else {return}
     if canPlayAudio {
       let audioNode = SKAudioNode(fileNamed: fileName)
       audioNode.autoplayLooped = false
@@ -45,7 +28,7 @@ class GameWorld {
       let audioRateAction = SKAction.sequence([.wait(forDuration: 0.001), .run { [unowned self] in
         self.canPlayAudio = true
       }])
-      audioNode.run(.sequence([.group([.changeVolume(to: volume, duration: volumeChangeSpeed), .play()]), .wait(forDuration: duration), .run {
+      audioNode.run(.sequence([.group([.changeVolume(to: volume, duration: volumeChangeSpeed), .play(), audioRateAction]), .wait(forDuration: duration), .run {
         audioNode.removeFromParent()
       }]))
     }
